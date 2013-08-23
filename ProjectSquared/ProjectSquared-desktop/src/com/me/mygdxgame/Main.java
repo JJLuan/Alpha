6b5ef0f386931863f39332b9dcff97d98f1b2020
JJLuan <JohnLuan96@gmail.com>
2013-08-23 02:11:07 -0700

    Its a test

--- ProjectSquared/ProjectSquared-desktop/src/com/me/mygdxgame/Main.java
+++ ProjectSquared/ProjectSquared-desktop/src/com/me/mygdxgame/Main.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
 	public static void main(String[] args) {
+		System.out.println("this is a test");
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "DOES THIS WORK?";
 		cfg.useGL20 = true;
