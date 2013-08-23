2d89c688943356e7d66cffd7011f597d2da71d51
Im-Laodicean <im.laodicean@gmail.com>
2013-08-23 15:08:17 -0400

    Test

--- ProjectSquared/ProjectSquared/src/com/me/entities/Entity.java
+++ ProjectSquared/ProjectSquared/src/com/me/entities/Entity.java
@@ -1,6 +1,12 @@
 package com.me.entities;
 
+import com.badlogic.gdx.math.Vector2;
+
 public abstract class Entity {
+	
+	protected Vector2 location;
+	protected int width,height;
+	
 	public Entity(){
 		
 	}
